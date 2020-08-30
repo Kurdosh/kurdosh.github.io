95ece11dda0af0c324c6e799df2b321c93369e66
Fedor <fediapolom@gmail.com>
2020-08-30 16:10:29 +0300

    trafalgar upd

--- trafalgar/js/main.js
+++ trafalgar/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    $('.reviews__content').slick();
+    $('.reviews__content').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_next.png"></button>',
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    arrows: false
+                }
+            },
+        ]
+    });
 });
