d36469c9ff5aa9c5859831cbafbca2aa9a4a523f
Fedor <fediapolom@gmail.com>
2020-08-30 16:19:37 +0300

    fix

--- trafalgar/js/main.js
+++ trafalgar/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.reviews__content').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_prev.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_next.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_next.png"></button>',
         responsive: [
             {
                 breakpoint: 800,
