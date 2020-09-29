f901fb4783806869a18ef6c50b6a70881dc37651
Fedor <fediapolom@gmail.com>
2020-09-29 18:06:56 +0300

    Taro

---
+++ taro/dist/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.review__slider').slick({
+        dots: true,
+        prevArrow: $('.prev-arrow'),
+        nextArrow: $('.next-arrow'),
+        dotsClass: "dots-style",
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        },]
+    });
+    $('.burger').click(function () {
+        $('.header__nav').toggleClass('header__nav_active');
+        $('.burger').toggleClass('burger_active');
+    });
+
+    $(window).scroll(function () {
+        var scrolled = $(this).scrollTop();
+        if ($(window).width() <= '995') {
+            if (scrolled >= 1000) {
+                $('.header__nav').addClass('header__nav_top');
+            }
+            if (scrolled <= 1000) {
+                $('.header__nav').removeClass('header__nav_top');
+            }
+        }
+    });
+});
